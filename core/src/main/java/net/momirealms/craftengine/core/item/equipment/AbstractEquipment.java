a4f58ab57c52ac3b9485376e0d81c09f6aa3f147
XiaoMoMi <littlemomi@qq.com>
2025-11-24 00:45:24 +0800

    修复其他插件区块生成

--- core/src/main/java/net/momirealms/craftengine/core/item/equipment/AbstractEquipment.java
+++ core/src/main/java/net/momirealms/craftengine/core/item/equipment/AbstractEquipment.java
@@ -2,11 +2,13 @@ package net.momirealms.craftengine.core.item.equipment;
 
 import net.momirealms.craftengine.core.util.Key;
 
+import java.util.Objects;
+
 public abstract class AbstractEquipment implements Equipment {
     protected final Key assetId;
 
     protected AbstractEquipment(Key assetId) {
-        this.assetId = assetId;
+        this.assetId = Objects.requireNonNull(assetId, "assetId cannot be null");
     }
 
     @Override
