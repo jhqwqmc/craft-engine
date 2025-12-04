94bc0e84824b10fbf0d94d7cdf81eba2c21cf684
XiaoMoMi <littlemomi@qq.com>
2025-12-05 01:51:43 +0800

    添加关闭物品栏函数

---
+++ core/src/main/java/net/momirealms/craftengine/core/plugin/context/function/CloseInventoryFunction.java
@@ -0,0 +1,50 @@
+package net.momirealms.craftengine.core.plugin.context.function;
+
+import net.momirealms.craftengine.core.entity.player.Player;
+import net.momirealms.craftengine.core.plugin.context.Condition;
+import net.momirealms.craftengine.core.plugin.context.Context;
+import net.momirealms.craftengine.core.plugin.context.parameter.DirectContextParameters;
+import net.momirealms.craftengine.core.plugin.context.selector.PlayerSelector;
+import net.momirealms.craftengine.core.plugin.context.selector.PlayerSelectors;
+import net.momirealms.craftengine.core.util.Key;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Map;
+
+public class CloseInventoryFunction<CTX extends Context> extends AbstractConditionalFunction<CTX> {
+    private final PlayerSelector<CTX> selector;
+
+    public CloseInventoryFunction(List<Condition<CTX>> predicates, @Nullable PlayerSelector<CTX> selector) {
+        super(predicates);
+        this.selector = selector;
+    }
+
+    @Override
+    public void runInternal(CTX ctx) {
+        if (this.selector == null) {
+            ctx.getOptionalParameter(DirectContextParameters.PLAYER).ifPresent(Player::closeInventory);
+        } else {
+            for (Player viewer : this.selector.get(ctx)) {
+                viewer.closeInventory();
+            }
+        }
+    }
+
+    @Override
+    public Key type() {
+        return CommonFunctions.TITLE;
+    }
+
+    public static class FactoryImpl<CTX extends Context> extends AbstractFactory<CTX> {
+
+        public FactoryImpl(java.util.function.Function<Map<String, Object>, Condition<CTX>> factory) {
+            super(factory);
+        }
+
+        @Override
+        public Function<CTX> create(Map<String, Object> arguments) {
+            return new CloseInventoryFunction<>(getPredicates(arguments), PlayerSelectors.fromObject(arguments.get("target"), conditionFactory()));
+        }
+    }
+}
