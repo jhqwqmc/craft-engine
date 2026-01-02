f4b46e96696796f59885b121b96d8cf30ff26766
jhqwqmc <jhcraft@qq.com>
2026-01-02 08:12:31 +0800

    清理代码

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/plugin/reflection/minecraft/CoreReflections.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/plugin/reflection/minecraft/CoreReflections.java
@@ -7,7 +7,6 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.DynamicOps;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelFuture;
-import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import net.momirealms.craftengine.bukkit.plugin.reflection.ReflectionInitException;
 import net.momirealms.craftengine.bukkit.util.BukkitReflectionUtils;
@@ -4698,23 +4697,4 @@ public final class CoreReflections {
             )
     );
 
-    public static final Field field$ChunkMap$entityMap = requireNonNull(
-            ReflectionUtils.getDeclaredField(
-                    clazz$ChunkMap, Int2ObjectMap.class, 0
-            )
-    );
-
-    public static final MethodHandle methodHandle$ChunkMap$entityMapGetter;
-    public static final MethodHandle methodHandle$ChunkMap$entityMapSetter;
-
-    static {
-        try {
-            methodHandle$ChunkMap$entityMapGetter = ReflectionUtils.unreflectGetter(field$ChunkMap$entityMap)
-                    .asType(MethodType.methodType(Int2ObjectMap.class, Object.class));
-            methodHandle$ChunkMap$entityMapSetter = requireNonNull(ReflectionUtils.unreflectSetter(field$ChunkMap$entityMap))
-                    .asType(MethodType.methodType(void.class, Object.class, Int2ObjectMap.class));
-        } catch (ReflectiveOperationException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
