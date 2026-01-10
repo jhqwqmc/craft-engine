d720c15209b2f0e0a076d06d6efbd05a11394091
XiaoMoMi <littlemomi@qq.com>
2026-01-10 22:40:17 +0800

    修改冷却变量实现

--- bukkit/compatibility/src/main/java/net/momirealms/craftengine/bukkit/compatibility/papi/CraftEngineExpansion.java
+++ bukkit/compatibility/src/main/java/net/momirealms/craftengine/bukkit/compatibility/papi/CraftEngineExpansion.java
@@ -5,10 +5,11 @@ import net.momirealms.craftengine.bukkit.api.BukkitAdaptors;
 import net.momirealms.craftengine.bukkit.plugin.user.BukkitServerPlayer;
 import net.momirealms.craftengine.core.plugin.CraftEngine;
 import net.momirealms.craftengine.core.plugin.context.CooldownData;
-import net.momirealms.craftengine.core.util.DurationFormatter;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
 
 public class CraftEngineExpansion extends PlaceholderExpansion {
     private final CraftEngine plugin;
@@ -26,7 +27,7 @@ public class CraftEngineExpansion extends PlaceholderExpansion {
     @NotNull
     @Override
     public String getAuthor() {
-        return "jhqwqmc";
+        return "XiaoMoMi";
     }
 
     @NotNull
@@ -43,42 +44,31 @@ public class CraftEngineExpansion extends PlaceholderExpansion {
     @Override
     public @Nullable String onPlaceholderRequest(Player bukkitPlayer, @NotNull String params) {
         BukkitServerPlayer player = bukkitPlayer != null ? BukkitAdaptors.adapt(bukkitPlayer) : null;
-        int index = params.indexOf('_');
-        String action = index > 0 ? params.substring(0, index) : params;
-        String[] param;
-        if (index > 0) {
-            String substring = params.substring(index + 1);
-            int i = substring.indexOf('|');
-            if (i > 0) {
-                param = new String[]{substring.substring(0, i), substring.substring(i + 1)};
-            } else {
-                param = new String[]{substring};
-            }
-        } else {
-            param = new String[0];
+        String[] split = params.split("_", 2);
+        if (split.length == 2) {
+            return switch (split[0]) {
+                case "cd", "cooldown" -> Optional.ofNullable(getCooldown(player, split[1])).orElse("0");
+                default -> null;
+            };
         }
-        return switch (action) {
-            case "cd", "cooldown" -> handleCooldown(player, param);
-            default -> null;
-        };
+        return null;
     }
 
-    @Nullable
-    private static String handleCooldown(@Nullable BukkitServerPlayer player, String[] param) {
-        if (player == null || param.length < 1) {
+    private static String getCooldown(BukkitServerPlayer player, String param) {
+        if (player == null) {
             return null;
         }
         CooldownData cooldown = player.cooldown();
         if (cooldown == null) {
             return null;
         }
-        Long ms = cooldown.getExpirationTime(param[0]);
+        Long ms = cooldown.getExpirationTime(param);
         if (ms == null) {
-            return "-1";
+            return null;
         }
         ms -= System.currentTimeMillis();
-        if (param.length >= 2) {
-            return DurationFormatter.of(param[1]).format(ms);
+        if (ms < 0) {
+            return null;
         }
         return String.valueOf(ms);
     }
