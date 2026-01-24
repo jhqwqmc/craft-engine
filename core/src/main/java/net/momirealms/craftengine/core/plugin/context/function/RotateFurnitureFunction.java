b0e5327b36c7d4e8fef0590a0e1edbe35219b8b0
jhqwqmc <jhcraft@qq.com>
2026-01-25 05:49:26 +0800

    修复家具在被移动中被反复调用移动导致的问题

--- core/src/main/java/net/momirealms/craftengine/core/plugin/context/function/RotateFurnitureFunction.java
+++ core/src/main/java/net/momirealms/craftengine/core/plugin/context/function/RotateFurnitureFunction.java
@@ -1,6 +1,7 @@
 package net.momirealms.craftengine.core.plugin.context.function;
 
 import net.momirealms.craftengine.core.entity.furniture.Furniture;
+import net.momirealms.craftengine.core.plugin.CraftEngine;
 import net.momirealms.craftengine.core.plugin.context.CommonFunctions;
 import net.momirealms.craftengine.core.plugin.context.Condition;
 import net.momirealms.craftengine.core.plugin.context.Context;
