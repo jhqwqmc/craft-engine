f5ba4abf2e6250dda312a323ed36dfe305a2023f
XiaoMoMi <70987828+Xiao-MoMi@users.noreply.github.com>
2026-07-14 20:55:27 +0800

    添加方块展示实体元素

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/block/entity/renderer/constant/BukkitBlockEntityElementConfigs.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/block/entity/renderer/constant/BukkitBlockEntityElementConfigs.java
@@ -7,6 +7,7 @@ import net.momirealms.craftengine.core.util.Key;
 public final class BukkitBlockEntityElementConfigs extends BlockEntityElementConfigs {
     public static final BlockEntityElementConfigType<ItemDisplayBlockEntityElement> ITEM_DISPLAY = register(Key.ce("item_display"), ItemDisplayBlockEntityElementConfig.FACTORY);
     public static final BlockEntityElementConfigType<TextDisplayBlockEntityElement> TEXT_DISPLAY = register(Key.ce("text_display"), TextDisplayBlockEntityElementConfig.FACTORY);
+    public static final BlockEntityElementConfigType<BlockDisplayBlockEntityElement> BLOCK_DISPLAY = register(Key.ce("block_display"), BlockDisplayBlockEntityElementConfig.FACTORY);
     public static final BlockEntityElementConfigType<ItemBlockEntityElement> ITEM = register(Key.ce("item"), ItemBlockEntityElementConfig.FACTORY);
     public static final BlockEntityElementConfigType<ArmorStandBlockEntityElement> ARMOR_STAND = register(Key.ce("armor_stand"), ArmorStandBlockEntityElementConfig.FACTORY);
 
--- core/src/main/java/net/momirealms/craftengine/core/block/entity/render/element/BlockEntityElementConfigs.java
+++ core/src/main/java/net/momirealms/craftengine/core/block/entity/render/element/BlockEntityElementConfigs.java
@@ -36,6 +36,8 @@ public abstract class BlockEntityElementConfigs {
                 return "text_display";
             } else if (section.containsKey("item")) {
                 return "item_display";
+            } else if (section.containsKey("block")) {
+                return "block_display";
             } else {
                 // 到这里必定抛出异常
                 return section.getNonNullString("type");
